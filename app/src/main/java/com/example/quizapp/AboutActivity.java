e50865213af653b4a38cd3c4c9c94e9562b2bbd8
sana1207 <sanafatema97@gmail.com>
2020-04-29 20:06:17 +0400

    First Commit

---
+++ app/src/main/java/com/example/quizapp/AboutActivity.java
@@ -0,0 +1,30 @@
+package com.example.quizapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+    public class AboutActivity extends AppCompatActivity {
+        Button btnRestart;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_about);
+
+            btnRestart = (Button) findViewById(R.id.button4);
+
+
+            btnRestart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in2 = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(in2);
+                }
+            });
+        }
+    }
+
+
